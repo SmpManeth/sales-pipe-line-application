6c3236be5fdd8556762676397933be2b9b28e95d
SmpManeth <smpmaneth@gmail.com>
2022-05-24 11:53:15 +0530

    paused

--- crm/settings.py
+++ crm/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'deals',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- deals/models.py
+++ deals/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Deal(models.Model):
+    contact_person = models.CharField(max_length=100, blank=False)
+    organization = models.CharField(max_length=100, blank=False)
+    title = models.CharField(max_length=100, blank=False)
+    value = models.IntegerField(default=0)
+    pipeline = models.CharField(max_length=100)
+    pipeline_stage = models.CharField(max_length=100)
+    expected_close_date = models.DateField()
+    visible_to = models.CharField(max_length=100)
+    mobile_no = models.CharField(max_length=100 ,blank=False)
+    work_mobile_no = models.CharField(max_length=100)
+
+    personal_email = models.CharField(max_length=100)
+    work_email = models.CharField(max_length=201000)
+
+
